c7451266bcf0b26b32f11db44d4926ab1faeaf25
Samuel-DeSantis <samuel.i.desantis@gmail.com>
2024-07-08 23:02:16 -0600

    Updating Sign In, Home, and trying to add Context Adjusted the styling of the Sign In. Laying out some of the sections for Home. Looking into adding Context to manage the application state for user sign in and sign out.

---
+++ src/pages/general/home/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <section className=''>
+      <h1 className='uppercase text-5xl md:text-7xl pb-8'>Design Faster</h1>
+      <p className='text-sm text-balance'>Develop cable & conduit Schedules faster and easier than before. Complex projects require a proper database to manage assocations and make sure updates are consistent across the board.</p>
+    </section>
+  )
+}
+
+export default Hero
---
+++ src/pages/general/home/components/TwoCol.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function TwoCol() {
+  return (
+    <section className='pt-96 text-center'>
+      <h1 className='text-3xl pb-20'>Why use Margo?</h1>
+      <div className='grid grid-cols-2 '>
+        <div>Stuff Here</div>
+        <div>Stuff Here</div>
+      </div>
+      
+    </section>
+  )
+}
+
+export default TwoCol
